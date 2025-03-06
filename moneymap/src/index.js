0909c049262464ec18d03aafc55da70f97d5a4f5
Ankit <work.ankitpokhrel@gmail.com>
2025-03-07 00:29:55 +0545

    edited

---
+++ moneymap/src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./assets/styles/tailwind.css";
+
+// layouts
+
+import Admin from "layouts/Admin.js";
+import Auth from "layouts/Auth.js";
+
+// views without layouts
+
+import Landing from "views/Landing.js";
+import Profile from "views/Profile.js";
+import Index from "views/Index.js";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      {/* add routes with layouts */}
+      <Route path="/admin/*" element={<Admin />} />
+      <Route path="/auth/*" element={<Auth />} />
+      {/* add routes without layouts */}
+      <Route path="/landing" element={<Landing />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/" element={<Index />} />
+      {/* add redirect for first page */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
